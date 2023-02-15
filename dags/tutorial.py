346842828fb37191bdfa5a225f9fc51742584c25
Adam Żbikowski <ajzbikowski@gmail.com>
2023-02-15 19:41:56 +0000

    FIx postgres connection

--- dags/tutorial.py
+++ dags/tutorial.py
@@ -25,12 +25,11 @@ dag = DAG(
 # Define the task: creating the table 'data' in the PostgreSQL database
 create_table_task = PostgresOperator(
     task_id='create_table',
-    postgres_conn_id='postgres_conn',
     sql="""
-        CREATE TABLE data (
-            Name text,
-            Age text,
-            Country text
+        CREATE TABLE IF NOT EXISTS data (
+            Name VARCHAR,
+            Age INT,
+            Country VARCHAR
         );
     """,
     dag=dag,
@@ -39,7 +38,6 @@ create_table_task = PostgresOperator(
 # Define the task: loading data from a CSV file into a PostgreSQL database
 load_data_task = PostgresOperator(
     task_id='load_data',
-    postgres_conn_id='postgres_conn',
     sql="""
         COPY data FROM './data/generated_data.csv' DELIMITER ',' CSV HEADER;
     """,
