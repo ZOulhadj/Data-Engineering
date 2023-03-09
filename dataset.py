62b70782524ec87870ab8c6163d368a74212f3e5
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2023-03-09 19:24:41 +0000

    Updated Python download script.

--- dataset.py
+++ dataset.py
@@ -1,8 +1,29 @@
 import requests
 
-url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv"
-name = "./data/housing_dataset.csv"
-dataset = requests.get(url, allow_redirects=True)
+datasets = {
+    "housing": "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.csv",
+}
+
+# Download and create dataset files
+def download_dataset(name, url, output_folder="./data/", output_format=".csv"):
+    # TODO: Need to handle situations where a dataset cannot be downloaded
+    dataset = requests.get(url, allow_redirects=True)
+    output_location = output_folder + name + output_format
+    open(output_location, "wb").write(dataset.content)
+
+    return output_location
 
 
-open(name, "wb").write(dataset.content)
+def main():
+    print("Starting dataset downloads")
+
+    for name in datasets:
+        print("Downloading {} dataset".format(name))
+        output = download_dataset(name, datasets[name])
+        print("Downloaded {} dataset into {}".format(name, output))
+
+    print("All datasets have been downloaded!")
+
+
+if __name__ == "__main__":
+    main()
