fae19e77d332061722e81cce69dd669faeaeb498
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2023-04-01 21:56:09 +0100

    SQL commands are now located in their own files which are loaded by the DAG pipeline.

--- plugins/includes/create_database.py
+++ plugins/includes/create_database.py
@@ -1,13 +1,13 @@
 from airflow import DAG
 from airflow.operators.postgres_operator import PostgresOperator
 
+# SELECT 'CREATE DATABASE prices_paid' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'prices_paid')
+
 def create_database(dag: DAG) -> PostgresOperator:
     task = PostgresOperator(
         task_id='create_database',
         postgres_conn_id='postgres_default',
-        sql="""
-            SELECT 'CREATE DATABASE prices_paid' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'prices_paid')
-        """,
+        sql="sql/create_database.sql",
         autocommit=True,
         dag=dag,
     )
--- plugins/includes/create_tables.py
+++ plugins/includes/create_tables.py
@@ -7,27 +7,7 @@ def create_table(dag: DAG) -> PostgresOperator:
     task = PostgresOperator(
         task_id='create_table',
         postgres_conn_id='postgres_default',
-        sql="""
-            \c prices_paid;
-
-            CREATE TABLE IF NOT EXISTS pp_complete (
-                transaction_id VARCHAR PRIMARY KEY,
-                price int,
-                transfer_date timestamp,
-                postcode text,
-                property_type char(1),
-                old_new char(1),
-                duration char(1),
-                address1 text,
-                address2 text,
-                street text,
-                locality text,
-                city text,
-                district text,
-                county text,
-                category char(1),
-                status char(1)
-            );""",
+        sql="sql/create_table.sql",
         dag=dag,
     )
 
