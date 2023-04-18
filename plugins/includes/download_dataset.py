f5cdcb6d7e54df728507522f1ab00f343b8a5baa
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2023-04-18 22:37:39 +0100

    Working on dataset downloading.

--- plugins/includes/download_dataset.py
+++ plugins/includes/download_dataset.py
@@ -1,13 +1,38 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-def print_data():
-    print("Downloading pp-complete dataset")
+import requests
 
-def download_pp_complete(dag: DAG) -> PythonOperator:
+
+
+# Download and create dataset files
+def download_dataset(name, url, output_folder="./data/", output_format=".csv"):
+    # TODO: Need to handle situations where a dataset cannot be downloaded
+    dataset = requests.get(url, allow_redirects=True)
+    output_location = output_folder + name + output_format
+    open(output_location, "wb").write(dataset.content)
+
+    return output_location
+
+
+def entry_point(**kwargs):
+    print("Starting dataset downloads")
+
+    datasets = kwargs["datasets"]
+
+    for name in datasets:
+        print("Downloading {} dataset".format(name))
+        output = download_dataset(name, datasets[name])
+        print("Downloaded {} dataset into {}".format(name, output))
+
+    print("All datasets have been downloaded!")
+
+
+def download_pp_complete(dag: DAG, datasets) -> PythonOperator:
     print_data_task = PythonOperator(
         task_id='download_pp_complete',
-        python_callable=print_data,
+        python_callable=entry_point,
+        op_kwargs={"datasets" : datasets},
         dag=dag,
     )
 
