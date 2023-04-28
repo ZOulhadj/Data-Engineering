9410b1d851d79893a57720d6a53529ee17ff839e
Zakariya Oulhadj <zakariyaoulhadj01@gmail.com>
2023-04-28 22:56:08 +0100

    Working on visualisation.

--- dags/3_visualizations.py
+++ dags/3_visualizations.py
@@ -1,12 +1,16 @@
-from __future__ import print_function
+import pandas as pd
 from datetime import datetime, timedelta
 from airflow.utils.dates import days_ago
+import pendulum
 from airflow import models
 from airflow.operators import python_operator
 from airflow.operators.dummy import DummyOperator
-import pandas as pd
 from sqlalchemy import create_engine
+from elasticsearch import Elasticsearch
+from airflow import models
 
+utc_date = days_ago(1)
+local_tz = pendulum.timezone("CET")
 
 default_dag_args = {
     'owner': 'you',
@@ -17,14 +21,45 @@ default_dag_args = {
     'retries': 0,
 }
 
-with models.DAG(
-        '3_visualizations',
-        schedule_interval=None,
-        max_active_runs=1,
-        catchup=False,
-        default_args=default_dag_args) as dag:
-      
-    start = DummyOperator(task_id='start', dag=dag)
-    end = DummyOperator(task_id='end', dag=dag)
 
-    start >>  end
+with models.DAG(
+    '3_visualizations',
+    schedule_interval=None,
+    max_active_runs=1,
+    catchup=False,
+    default_args=default_dag_args) as dag:
+    
+
+    def queryPostgres():
+        engine = create_engine('postgresql+psycopg2://airflow:airflow@postgres/airflow')
+        df = pd.read_sql_query('select * from "price_housing_data"',con=engine)
+        print(df.head())
+        df.to_csv('postgresqldata.csv')
+        print("-------Data Saved------")
+
+    def insertElasticseearch():
+        es = Elasticsearch("my_elasticsearch")
+        df=pd.read_csv('postgresqldata.csv')
+        for i,r in df.iterrows():
+            doc=r.to_json()
+            res=es.index(index="frompostgresql", doc_type="doc",body=doc)
+            print(res)
+
+    getData = python_operator.PythonOperator(
+        task_id='QueryPostgreSQL', 
+        python_callable=queryPostgres)
+
+    insertData = python_operator.PythonOperator(
+        task_id='InsertDataElasticsearch', 
+        python_callable=insertElasticseearch)
+
+    start = DummyOperator(
+        task_id='start',
+        dag=dag)
+    
+    end = DummyOperator(
+        task_id='end',
+        dag=dag
+    )
+
+    start >> getData >> insertData >> end
